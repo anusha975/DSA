4192aacecd8a526124e24bf904a5f05fd8e5e9cd
anusha975 <canusha835@gmail.com>
2025-11-13 17:56:49 +0530

    Add maxProfit method for stock trading problem
    Implement maxProfit method to calculate maximum profit with cooldown.

---
+++ 309 Best Time to Buy and Sell Stock with Cooldown/309 Best Time to Buy and Sell Stock .py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        buyPrice = prices[0]
+
+        for price in prices[1:]:
+            if price < buyPrice :
+                buyPrice = price
+            elif price-buyPrice > maxProfit:
+                maxProfit = price-buyPrice
+        return maxProfit
+
+
+        
+        
