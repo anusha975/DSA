16d67a4f455e2b343c3462ffc4ec45e2c4003da2
anusha975 <canusha835@gmail.com>
2025-11-13 18:18:41 +0530

    Create 277 Find the Celebrity.java

---
+++ 277 Find the Celebrity/277 Find the Celebrity.java
@@ -0,0 +1,40 @@
+
+
+
+public class Solution extends Relation {
+    
+    private int numOfPeople;
+    
+    public int findCelebrity(int n) {
+        numOfPeople = n;
+        int celebCandiate = 0;
+        
+        for(int i=0; i<n; i++){
+            if(knows(celebCandiate, i)){
+                celebCandiate = i;
+            }
+        }
+        
+        if(isCelebrity(celebCandiate)){
+            return celebCandiate;
+        }
+        
+        return -1;
+        
+    }
+    
+    public boolean isCelebrity(int i){
+        
+        for(int j=0; j<numOfPeople; j++){
+            if(i==j){
+                continue;
+            }
+            if(knows(i,j) || !knows(j,i)){
+                return false;
+            }
+        }
+        
+        return true;
+        
+    }
+}
